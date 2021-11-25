9f382607633fb6da280932bcfb3ed5bbf489e89a
limchihi <limchihi@foxmail.com>
2021-11-26 02:33:36 +0800

    Add id subscript to AdjacencyListGraph

--- Sources/InteractGraph/Utils/AdjacencyListGraph.swift
+++ Sources/InteractGraph/Utils/AdjacencyListGraph.swift
@@ -66,8 +66,25 @@ internal struct AdjacencyListGraph<Element>: Collection {
         return IndexPath(row: storage.endIndex - 1, column: 0)
     }
     
+    @discardableResult
+    internal mutating func remove(at indexPath: IndexPath) -> Element {
+        storage[indexPath.row].remove(at: indexPath.column)
+    }
+    
     internal func flatForEach(_ block: (Element) throws -> ()) rethrows {
         try storage.forEach { try $0.forEach { try block($0) }}
+    }
+    
+    internal func flatForEach(_ block: (Element, IndexPath, inout Bool) throws -> ()) rethrows {
+        var stop = false
+        for row in storage.indices {
+            for column in storage[row].indices {
+                try block(storage[row][column], IndexPath(row: row, column: column), &stop)
+                if stop {
+                    return
+                }
+            }
+        }
     }
 
     internal func flatMap<T>(_ transform: (Element) throws -> T) rethrows -> [T] {
@@ -108,6 +125,46 @@ internal struct AdjacencyListGraph<Element>: Collection {
             self.column = indexPath.column
         }
         
+    }
+    
+}
+
+
+extension AdjacencyListGraph where Element: Identifiable {
+    
+    internal func firstIndex(_ id: Element.ID) -> IndexPath? {
+        var result: IndexPath?
+        flatForEach { element, indexPath, stop in
+            if element.id == id {
+                result = indexPath
+            }
+        }
+        return result
+    }
+    
+    internal func first(_ id: Element.ID) -> Element? {
+        var result: Element?
+        flatForEach { element, indexPath, stop in
+            if element.id == id {
+                result = element
+            }
+        }
+        return result
+    }
+    
+    internal subscript(id: Element.ID) -> Element? {
+        get {
+            first(id)
+        }
+        set {
+            if let index = firstIndex(id) {
+                if let newValue = newValue {
+                    self[index] = newValue
+                } else {
+                    remove(at: index)
+                }
+            }
+        }
     }
     
 }
