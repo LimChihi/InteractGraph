c5d9755890bb3b70e55da22a9e5e230c7048db76
limchihi <limchihi@foxmail.com>
2022-01-09 00:29:02 +0800

    Import GraphViz

--- Package.swift
+++ Package.swift
@@ -23,10 +23,19 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .systemLibrary(
+            name: "Clibgraphviz",
+            pkgConfig: "libcgraph",
+            providers: [
+              .brew(["graphviz"]),
+              .apt(["graphviz-dev"]),
+            ]
+        ),
         .target(
             name: "InteractGraph",
             dependencies: [
-                .product(name: "Collections", package: "swift-collections")
+                .product(name: "Collections", package: "swift-collections"),
+                "Clibgraphviz"
             ]),
         .testTarget(
             name: "InteractGraphTests",
